f0c4be5b26dffa27154add0f3bb4091451e8382e
Sakshi Sharma <ThisIsSakshiSharma@gmail.com>
2023-06-23 05:31:30 +0530

    updated dark mode

--- dark.py
+++ dark.py
@@ -45,7 +45,7 @@ def get_image_tag(repo):
     if not is_image:
         sys.exit(1)
     img_src = image.download_url
-    img_tag = f"<img src=\"{img_src}#gh-dark-mode-only\" height={HEIGHT} width={WIDTH} align={ALIGN} alt={IMG_ALT} />"
+    img_tag = f"<img src=\"{img_src}#gh-dark-mode-only\" title=\"Hello! 😇💙\" height={HEIGHT} width={WIDTH} align={ALIGN} alt={IMG_ALT} />"
     return img_tag
 
 def decode_readme(data: str) -> str:
